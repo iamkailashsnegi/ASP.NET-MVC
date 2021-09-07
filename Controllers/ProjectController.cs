859e1369658573683a6760140ab39cfd003e6cd3
iamkailashsnegi <kailashsinghnegi02@gmail.com>
2021-09-07 12:05:37 +0530

    //final tuesday

--- Controllers/ProjectController.cs
+++ Controllers/ProjectController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,11 +10,23 @@ namespace MyProjectWebApp.Controllers
 {
     public class ProjectController : Controller
     {
+        private SqlConnection con;
+
+        private void Connection()
+        {
+            string _conString = ConfigurationManager.ConnectionStrings["DbConnection"].ConnectionString;
+            con = new SqlConnection(_conString);
+
+        }
         // GET: Project
         public ActionResult Index()
         {
             return View();
         }
-
+        public ActionResult Edit(int Id)
+        {
+            return View();
+        }
+       
     }
 }
